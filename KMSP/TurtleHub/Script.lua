8acb52ad990223946721da79785dde17ff3bae33
Terminated <117529823+Sidhsksjsjsh@users.noreply.github.com>
2023-02-22 07:20:19 +0900

    Update Script.lua

--- KMSP/TurtleHub/Script.lua
+++ KMSP/TurtleHub/Script.lua
@@ -24,6 +24,7 @@ local Tab_2 = ui:new("Player")
 local Tab_3 = ui:new("Visual")
 local Tab_4 = ui:new("Misc")
 local Tab_8 = ui:new("Daily Reward")
+local Tab_9 = ui:new("Save")
 
 Tab_1:Ping()
 Tab_1:FPS()
@@ -54,6 +55,106 @@ function Magic_Damage()
             end
         end
     end
+
+local Domain = {
+                    Domain = Instance.new("ScreenGui"),
+                    UITextSizeConstraint_13 = Instance.new("UITextSizeConstraint"),
+                    FriendJoined = Instance.new("Frame"),
+                    UICorner_66 = Instance.new("UICorner"),
+                    UICorner_67 = Instance.new("UICorner"),
+                    Textjoin = Instance.new("TextLabel"),
+                    Other = Instance.new("Frame"),
+                    Avatarjoin = Instance.new("ImageLabel")
+}
+
+Domain.Domain.Name = "Domain"
+Domain.Domain.Parent = game:GetService("CoreGui")
+Domain.Domain.ResetOnSpawn = false
+
+Domain.Other.Name = "Other"
+Domain.Other.Parent = Domain.Domain
+Domain.Other.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Domain.Other.BackgroundTransparency = 1.000
+Domain.Other.BorderSizePixel = 0
+Domain.Other.Position = UDim2.new(0.966000021, 0, 0.261999995, 0)
+Domain.Other.Size = UDim2.new(0.0430000015, 0, 0.474999994, 0)
+
+Domain.FriendJoined.Name = "FriendJoined"
+Domain.FriendJoined.Parent = Domain.Other
+Domain.FriendJoined.BackgroundColor3 = Color3.fromRGB(31, 31, 31)
+Domain.FriendJoined.BorderSizePixel = 0
+Domain.FriendJoined.Position = UDim2.new(-4.58847857, 0, 1.30335116, 0)
+Domain.FriendJoined.Size = UDim2.new(4.92762661, 0, 0.195112452, 0)
+Domain.FriendJoined.Visible = false
+ 
+Domain.UICorner_66.CornerRadius = UDim.new(0, 10)
+Domain.UICorner_66.Parent = Domain.FriendJoined
+ 
+Domain.Textjoin.Name = "Textjoin"
+Domain.Textjoin.Parent = Domain.FriendJoined
+Domain.Textjoin.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Domain.Textjoin.BackgroundTransparency = 1.000
+Domain.Textjoin.BorderSizePixel = 0
+Domain.Textjoin.Position = UDim2.new(0.26033023, 0, 0.25, 0)
+Domain.Textjoin.Size = UDim2.new(0.685603261, 0, 0.5, 0)
+Domain.Textjoin.Font = Enum.Font.GothamBold
+Domain.Textjoin.Text = "Your friend, usernameusername has joined the server"
+Domain.Textjoin.TextColor3 = Color3.fromRGB(255, 255, 255)
+Domain.Textjoin.TextScaled = true
+Domain.Textjoin.TextSize = 14.000
+Domain.Textjoin.TextWrapped = true
+Domain.Textjoin.TextXAlignment = Enum.TextXAlignment.Left
+ 
+Domain.UITextSizeConstraint_13.Parent = Domain.Textjoin
+Domain.UITextSizeConstraint_13.MaxTextSize = 20
+ 
+Domain.Avatarjoin.Name = "Avatarjoin"
+Domain.Avatarjoin.Parent = Domain.FriendJoined
+Domain.Avatarjoin.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Domain.Avatarjoin.BackgroundTransparency = 1.000
+Domain.Avatarjoin.BorderSizePixel = 0
+Domain.Avatarjoin.Position = UDim2.new(0.0651319921, 0, 0.189999998, 0)
+Domain.Avatarjoin.Size = UDim2.new(0.150188312, 0, 0.606190205, 0)
+Domain.Avatarjoin.Image = "rbxasset://textures/ui/GuiImagePlaceholder.png"
+
+Domain.UICorner_67.CornerRadius = UDim.new(1, 20)
+Domain.UICorner_67.Parent = Domain.Avatarjoin
+
+function ProtocolNotify(A1)
+Domain.FriendJoined.Visible = true
+    Domain.FriendJoined.Size = UDim2.new(0,0,0.195,0)
+    Domain.FriendJoined.Textjoin.Text = A1
+    Domain.FriendJoined.Textjoin.TextTransparency = 1
+    Domain.FriendJoined.Avatarjoin.ImageTransparency = 1
+    local transitionInfo = TweenInfo.new(0.65, Enum.EasingStyle.Quint)
+    local tween = game:GetService("TweenService"):Create(Domain.FriendJoined, transitionInfo, {Size = UDim2.new(4.928, 0, 0.195, 0)})
+    tween:Play()
+    wait(0.5)
+    local transitionInfo = TweenInfo.new(0.25, Enum.EasingStyle.Quint)
+    local tween = game:GetService("TweenService"):Create(Domain.FriendJoined.Avatarjoin, transitionInfo, {BackgroundTransparency = 1})
+    tween:Play()
+    local transitionInfo = TweenInfo.new(0.45, Enum.EasingStyle.Quint)
+    local tween = game:GetService("TweenService"):Create(Domain.FriendJoined.Textjoin, transitionInfo, {TextTransparency = 0})
+    tween:Play()
+    local transitionInfo = TweenInfo.new(0.45, Enum.EasingStyle.Quint)
+    local tween = game:GetService("TweenService"):Create(Domain.FriendJoined.Avatarjoin, transitionInfo, {ImageTransparency = 0})
+    tween:Play()
+    wait(4)
+    local transitionInfo = TweenInfo.new(0.65, Enum.EasingStyle.Quint)
+    local tween = game:GetService("TweenService"):Create(Domain.FriendJoined.Textjoin, transitionInfo, {TextTransparency = 1})
+    tween:Play()
+    local transitionInfo = TweenInfo.new(0.65, Enum.EasingStyle.Quint)
+    local tween = game:GetService("TweenService"):Create(Domain.FriendJoined.Avatarjoin, transitionInfo, {ImageTransparency = 1})
+    tween:Play()
+    local transitionInfo = TweenInfo.new(0.65, Enum.EasingStyle.Quint)
+    local tween = game:GetService("TweenService"):Create(Domain.FriendJoined.Avatarjoin, transitionInfo, {BackgroundTransparency = 1})
+    tween:Play()
+    wait(0.5)
+    local transitionInfo = TweenInfo.new(0.65, Enum.EasingStyle.Quint)
+    local tween = game:GetService("TweenService"):Create(Domain.FriendJoined, transitionInfo, {Size = UDim2.new(4.928, 0, 0, 0)})
+    tween:Play()
+end
+
 
 --// Player Functions \\--
 function EnableClickTp()
@@ -105,9 +206,61 @@ function SpyChat()
     loadstring(game:HttpGet("https://github.com/CosmoCircle/scripts/blob/main/Spy_Chat", true))()
 end
 
+function ProtocolSaveSettings(file, config)
+       writefile(file, config)
+end
+
+function ProtocolReadSettings(file)
+       readfile(file)
+end
+
+function ProtocolCheckFile(file)
+       isfile(file)
+end
+
+local ProtocolTrigger_1 = false
+local ProtocolTrigger_2 = false
+local ProtocolTrigger_3 = false
+local ProtocolFileType = ""
+
+
+function LoadSettings()
+local Initializing_1 = ProtocolCheckFile("Autofarm_Waves_Setting_1." .. ProtocolFileType)
+local Initializing_2 = ProtocolCheckFile("Autofarm_Waves_Setting_2." .. ProtocolFileType)
+local Initializing_3 = ProtocolCheckFile("Auto_Damage_Setting." .. ProtocolFileType)
+
+if Initializing_1 and Initializing_2 and Initializing_3 then
+local Check_1 = ProtocolReadSettings("Autofarm_Waves_Setting_1." .. ProtocolFileType)
+local Check_2 = ProtocolReadSettings("Autofarm_Waves_Setting_2." .. ProtocolFileType)
+local Check_3 = ProtocolReadSettings("Auto_Damage_Setting." .. ProtocolFileType)
+ProtocolTrigger_1 = Check_1
+ProtocolTrigger_2 = Check_2
+ProtocolTrigger_3 = Check_3
+else
+ProtocolNotify("You didn't save your settings here, I hope you saved them first")
+end
+end
+
+Tab_9:CreateDropdown("File type", {"txt","json","html","lua"}, function(currentOption)
+     ProtocolFileType = currentOption
+end)
+
+Tab_9:CreateButton("save all toggles settings", function()
+  if ProtocolFileType == "" or ProtocolFileType == nil then
+       ProtocolNotify("please select the file type first")
+  else
+       ProtocolSaveSettings("Autofarm_Waves_Setting_1." .. ProtocolFileType, ProtocolTrigger_1)
+       ProtocolSaveSettings("Autofarm_Waves_Setting_2." .. ProtocolFileType, ProtocolTrigger_2)
+       ProtocolSaveSettings("Auto_Damage_Setting." .. ProtocolFileType, ProtocolTrigger_3)
+  end
+end)
+
+Tab_9:CreateButton("Load all toggles settings", function()
+LoadSettings()
+end)
 
 --// Main Contents \\--
-Tab_1:CreateToggle("Autofarm Waves (teleport)", false, function(state)
+Tab_1:CreateToggle("Autofarm Waves (teleport)", ProtocolTrigger_1, function(state)
     if state then
         WaveFarm = true
         while WaveFarm == true do
@@ -119,7 +272,7 @@ Tab_1:CreateToggle("Autofarm Waves (teleport)", false, function(state)
     end
 end)
 
-Tab_1:CreateToggle("Autofarm Waves (bring)", false, function(state)
+Tab_1:CreateToggle("Autofarm Waves (bring)", ProtocolTrigger_2, function(state)
     if state then
         BringWaveFarm = true
         while WaveFarm == true do
@@ -131,7 +284,7 @@ Tab_1:CreateToggle("Autofarm Waves (bring)", false, function(state)
     end
 end)
 
-Tab_1:CreateToggle("auto damage", false, function(state)
+Tab_1:CreateToggle("auto damage", ProtocolTrigger_3, function(state)
     if state then
         autoDamage = true
         while autoDamage == true do
@@ -316,5 +469,26 @@ end)
 
 Tab_4:CreateButton("Psy Hub", function()
 loadstring(game:GetObjects("rbxassetid://3014051754")[1].Source)()
+end)
+
+Tab_4:CreateButton("Anti-Kick", function()
+local mt = getrawmetatable(game)
+local old = mt.__namecall
+local protect = newcclosure or protect_function
+
+if not protect then
+protect = function(f) return f end
+end
+
+setreadonly(mt, false)
+mt.__namecall = protect(function(self, ...)
+local method = getnamecallmethod()
+if method == "Kick" then
+wait(9e9)
+return
+end
+return old(self, ...)
+end)
+hookfunction(game:GetService("Players").LocalPlayer.Kick,protect(function() wait(9e9) end))
 end)
 -- # test
